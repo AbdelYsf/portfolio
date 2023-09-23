67e0eb50c97328c945f8bb06c1657833c14119b9
AbdelYsf <abdel.ysf@gmail.com>
2023-09-23 22:35:50 +0100

    Update Summary.tsx

--- src/components/Summary.tsx
+++ src/components/Summary.tsx
@@ -7,9 +7,9 @@ export const Summary = (props:props)=>{
 
     return (
         < >
-            <div className={` text-center w-1/2 font-serif font-bold ${props.darkMode ? 'text-teal-100' : ''}`}>
+            <div className={` text-center sm:w-1/2 w-full font-serif font-bold ${props.darkMode ? 'text-teal-100' : ''}`}>
 
-                 Hi there <span className="text-2xl  handshaking">👋 </span>! I'm Abdellah EL YOUSSFI ALAOUI, a dedicated 🛠️ software crafter with a
+                 Hi there <span className="text-2xl">👋 </span>! I'm Abdellah EL YOUSSFI ALAOUI, a dedicated 🛠️ software crafter with a
                 passion for 🚀 product engineering and an insatiable appetite for 📚 learning.
             </div>
         </>
